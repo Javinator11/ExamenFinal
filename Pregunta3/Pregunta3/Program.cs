a68fe584a608ef1da8126c52632d5d575d22de40
Javinator11 <sosa.puga.javier@gmail.com>
2019-05-16 12:56:25 -0600

    Finished

--- Pregunta3/Pregunta3/Program.cs
+++ Pregunta3/Pregunta3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Pregunta3.Classes;
 
 namespace Pregunta3
 {
@@ -10,6 +11,38 @@ namespace Pregunta3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" Escriba su nombre ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine(" Escriba su apellido ");
+            string apellido = Console.ReadLine();
+
+            //char[] Nombre = new char[10];
+            //char[] Apellido = new char[10];
+
+            char[] Nombre = nombre.ToCharArray();
+            char[] Apellido = apellido.ToCharArray();
+
+            //if ()
+            //{
+            //    ExceptionNum exceptionNum = new ExceptionNum(message: string.Format(" Nombre/Apellido no valido "));
+            //    throw exceptionNum;
+            //}
+
+            if (Nombre.Length >= 2)
+            {
+                Console.WriteLine(" Nombre/Apellido no valido ");
+            }
+            else
+            {
+                if (Apellido.Length >= 2)
+                {
+                    Console.WriteLine(" Nombre/Apellido no valido ");
+                }
+                Console.ReadKey();
+            }
+
+
         }
     }
 }
---
+++ Pregunta4/Pregunta4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             class SelectionSort
+        {
+            public static void selectionSort<T>(ref T[] array) where T : IComparable
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    T key;
+
+                    key = array[i];
+                    int min = key;
+                    int minId = i;
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        if (array[j] < min)
+                        {
+                            min = array[j];
+                            minId = j;
+                        }
+                    }
+                    // swapping
+                    int temp = array[i];
+                    array[i] = min;
+                    array[minId] = temp;
+                }
+            }
+        }
+    }
+    }
+}
